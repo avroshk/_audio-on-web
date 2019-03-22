ea66bbb8c9ac768eaa400a1dd65dfb9613573489
Avrosh K <avroshkumar@gmail.com>
2019-03-21 17:25:12 -0700

    Check Web Audio Support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const IS_WEB_AUDIO_SUPPORTED = window.AudioContext || window.webkitAudioContext ? true : false
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          audio-on-web
+            Is Web Audio Supported?
+            <div>{IS_WEB_AUDIO_SUPPORTED ? "😊" : "☹️"}</div>
         </header>
       </div>
     );
